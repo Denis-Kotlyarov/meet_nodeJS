c5faf3077df5946894be03d8b3cb4b51a69e0527
Denis <156504836+Denis-Kotlyarov@users.noreply.github.com>
2024-05-20 19:46:37 +0300

    Я то думал просто числа вывести, а простые числа оказывается это термни. Срочно исправляем!

--- first_script.js
+++ first_script.js
@@ -1,6 +1,18 @@
 const m = 25;
 const n = 50;
 
-for (i = m; i <= n; i++) {
-    console.log(i);
+function primeNum(num) {
+    if (num <= 3) return true;
+    if ((num <= 1) || (num % 2 === 0 || num % 3 === 0)) return false;
+
+    for (let i = 5; i * i <= num; i += 6) {
+        if (num % i === 0 || num % (i + 2) === 0) return false;
+    }
+    return true;
+}
+
+for (let i = m; i <= n; i++) {
+    if (primeNum(i)) {
+        console.log(i);
+    }
 }
